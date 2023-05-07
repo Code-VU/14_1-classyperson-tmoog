7fcaca8cdd1cfe279bdafe44d56ef817331348d8
Taylor Moog <taylormoog@vu.com>
2023-05-07 16:05:07 -0500

    created the class person and then the 4 methods within that class

--- classy_person.py
+++ classy_person.py
@@ -8,6 +8,32 @@ don't forget the use of 'self' and to have the methods:
 '''
 
 class Person:
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+    
+    def increase_age(self):
+        self.age = self.age + 1
+    
+    def say_greeting(self):
+        print(f'Hello world! My name is {self.name}!')
+    
+    def count_to_age(self):
+        lst = []
+        count = 1
+        while count <= self.age:
+            lst.append(count)
+            count += 1
+        for x in lst:
+            print(x)
+
+#taylor = Person(26, 'Taylor')
+#print(taylor.age)
+#taylor.say_greeting()
+#taylor.increase_age()
+#print(taylor.age)
+#taylor.count_to_age()
+
 
 
 
